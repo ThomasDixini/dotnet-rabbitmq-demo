03856310d3621d0453655f2fe02e5fb7b999c797
ThomasDixini <thomasdixini.dev@gmail.com>
2026-04-16 20:57:39 -0300

    Adding binding keys to the examples

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -1,21 +1,17 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-var factory = new ConnectionFactory() { HostName = "localhost" };
+var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
 
-var message = GetMessage(args);
+var severity = (args.Length > 0) ? args[0] : "info";
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
 var body = Encoding.UTF8.GetBytes(message);
-await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, body: body);
+await channel.BasicPublishAsync(exchange: "direct_logs", routingKey: severity, body: body);
 
-Console.WriteLine(" [x] Sent {0}", message);
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
-
-static string GetMessage(string[] args)
-{
-    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
-}
--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -2,24 +2,41 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
 var factory = new ConnectionFactory { HostName = "localhost" };
+
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
 
-QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+// declare a server-named queue
+var queueDeclareResult = await channel.QueueDeclareAsync();
 string queueName = queueDeclareResult.QueueName;
-await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
 
-Console.WriteLine(" [*] Waiting for logs.");
+foreach (string? severity in args)
+{
+    await channel.QueueBindAsync(queue: queueName, exchange: "direct_logs", routingKey: severity);
+}
+
+Console.WriteLine(" [*] Waiting for messages.");
 
 var consumer = new AsyncEventingBasicConsumer(channel);
 consumer.ReceivedAsync += (model, ea) =>
 {
-    byte[] body = ea.Body.ToArray();
+    var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($" [x] {message}");
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
     return Task.CompletedTask;
 };
 
