1abe0582a47c9e0172517916261ac73ca06d1afa
ThomasDixini <thomasdixini.dev@gmail.com>
2026-04-16 21:03:08 -0300

    Working with topic exchanges

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -5,13 +5,10 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
+await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
 
-var severity = (args.Length > 0) ? args[0] : "info";
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
 var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
 var body = Encoding.UTF8.GetBytes(message);
-await channel.BasicPublishAsync(exchange: "direct_logs", routingKey: severity, body: body);
-
-Console.WriteLine($" [x] Sent '{severity}':'{message}'");
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
+await channel.BasicPublishAsync(exchange: "topic_logs", routingKey: routingKey, body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
